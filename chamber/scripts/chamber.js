25de9b55abe720aaa8951ad526b32883c435236c
u186335381 <u186335381@byui.edu>
2023-06-17 22:17:42 -0300

    Added home page chamber of commerce

---
+++ chamber/scripts/chamber.js
@@ -0,0 +1,27 @@
+function resizeHandler(event) {
+    if (window.innerWidth >= 500) {
+        const menuElements = document.querySelector(".nav-sites")
+        menuElements.style.visibility = 'visible'
+    }
+}
+
+
+const toggleMenu = () => {
+    const menuElements = document.querySelector(".nav-sites")
+    const menuButton = document.querySelector(".btn-hamburguer-menu")
+    if (menuElements.style.visibility === 'hidden') {
+        menuElements.style.visibility = 'visible'
+        menuButton.src = "images/btn-close-menu.png"
+    } else {
+        menuElements.style.visibility = 'hidden'
+        menuButton.src = "images/btn-open-menu.png"
+    }
+}
+
+const updateLastModifiedFooter = () => {
+    const pLastModified = document.getElementById("lastModified")
+    pLastModified.innerHTML = pLastModified.innerHTML.replace("{lastModification}", new Intl.DateTimeFormat('en-US', { dateStyle: 'short', timeStyle: 'short', timeZone: 'UTC' }).format(new Date()))
+}
+
+updateLastModifiedFooter()
+window.addEventListener("resize", resizeHandler)
