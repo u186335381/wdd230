fbce0d6ca048412720006b87153f757dfc13227e
u186335381 <u186335381@byui.edu>
2023-07-14 16:46:57 -0300

    feat: Completing week 04 assignment

--- scripts/form.js
+++ scripts/form.js
@@ -26,3 +26,17 @@ function checkPasswords() {
         message.computedStyleMap.visibility = "hidden"
     }
 }
+
+
+const updateCurrentYearFooter = () => {
+    const pCopyright = document.getElementById("p_copyright_and_information")
+    pCopyright.innerHTML = pCopyright.innerHTML.replace("{year}", new Date().getFullYear())
+}
+
+const updateLastModifiedFooter = () => {
+    const pLastModified = document.getElementById("lastModified")
+    pLastModified.innerHTML = pLastModified.innerHTML.replace("{lastModification}", new Intl.DateTimeFormat('en-US', { dateStyle: 'short', timeStyle: 'short', timeZone: 'UTC' }).format(new Date()))
+}
+
+updateCurrentYearFooter()
+updateLastModifiedFooter()
