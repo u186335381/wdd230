ad4db67ef357c0d487f8ffac0d8796592a2bfecb
u186335381 <u186335381@byui.edu>
2023-07-15 20:51:36 -0300

    feat: Added modifications for week 05 chamber

--- chamber/scripts/directory.js
+++ chamber/scripts/directory.js
@@ -1,3 +1,17 @@
+const buttonGrid = document.getElementById('toggle-grid')
+const buttonList = document.getElementById('toggle-list')
+const sectionMembers = document.querySelector('.article-grid')
+
+buttonGrid.addEventListener('click', () => {
+    sectionMembers.classList.add('article-grid')
+    sectionMembers.classList.remove('article-list')
+})
+
+buttonList.addEventListener('click', () => {
+    sectionMembers.classList.add('article-list')
+    sectionMembers.classList.remove('article-grid')
+})
+
 async function getLinks() {
     const body = await fetch('https://u186335381.github.io/wdd230/chamber/data/members.json')
     const data = await body.json()
@@ -6,8 +20,8 @@ async function getLinks() {
 
 function displayLinks(members) {
     const article = document.querySelector(".article-grid")
-    article.innerHTML = members.members.map(member => `<section>
-        <img src="images/${member.imageFileName}" alt="Company ${member.name}" style="width: 100%;">
+    article.innerHTML = members.members.map(member => `<section class="members-section">
+        <img src="images/${member.imageFileName}" alt="Company ${member.name}">
         <h3>${member.name}</h3>
         <p>${member.address}</p>
         <p>${member.phone}</p>
